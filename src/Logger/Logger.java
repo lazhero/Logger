6ecd8bc17b17feb78bb8849d73813bac1f590905
lazhero <54720338+lazhero@users.noreply.github.com>
2020-03-11 11:54:37 -0600

    Creates the ErrorReport and UpdateError, to get and set new exceptions

--- src/Logger/Logger.java
+++ src/Logger/Logger.java
@@ -1,10 +1,33 @@
 package Logger;
 
-import java.io.File;
-public class Logger {
+import Filehadling.FilesBuider;
+import Filehadling.Reader;
+import Filehadling.Writter;
+import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
+import java.util.ArrayList;
+
+public class Logger {
+    File file;
+    ArrayList<String> ErrorList=new ArrayList<String>();
     public Logger(){
 
+        FilesBuider FBuilder=FilesBuider.InstanceCreator();
+        file=FBuilder.BuildFile("Logger");
+    }
+    public void ErrorReport(@NotNull Exception e){
+        String text=e.getMessage();
+        if(!text.equalsIgnoreCase("Salto de linea")){
+            Writter.Write(file,text);
+        }
 
     }
+    public void UpdateError(){
+        ErrorList= Reader.Read(file);
+    }
+    public void nextline(){
+        Writter.Write(file,"Salto de linea");
+    }
+
 }
